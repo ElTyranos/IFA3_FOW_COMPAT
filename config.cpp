b8cd0747c725dd0fb7b337fd81afec087189b825
ElTyranos <pierre.cazard@gmail.com>
2016-11-14 17:10:26 +0100

    ~ Initital commit

---
+++ config.cpp
@@ -0,0 +1,14 @@
+class CfgPatches
+{
+	class IFA3_FOW_Compat
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"WW2_Assets_c_Weapons_Misc_c_Weapons","fow_weapons_c"};
+		magazines[] = {};
+		ammo[] = {};
+	};
+};
+
+#include "CfgWeapons.hpp"
